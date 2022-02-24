e3e8b8d427a965998b62ea7c575d9091db59ebd5
Peter <peterleite.dev@gmail.com>
2022-02-24 13:14:43 -0300

    componentes propriedades e estados

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { Tweet} from "./components/Tweet"
+import { useState} from "react";
+import { Tweet } from "./components/Tweet"
 
 function App() {
-  return <Tweet />
+const [tweets, setTweets]  = useState<string[]>([
+  'Tweet 1',
+  'Tweet 2',
+  'Tweet 3',
+  'Tweet 4',
+])
+
+function createTweets(){
+  setTweets([...tweets, 'twitewewas'])
+}
+
+return (
+
+  <div>
+  {tweets.map(tweet => {
+    return <Tweet text={tweet} />
+  })}
+
+  <button onClick={createTweets}>Adicionar Tweet</button>
+  </div>
+  );
 }
 
 export default App
